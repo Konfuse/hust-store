1cf9850c3b4e194bd0c2c6a5d347346108f53bc5
Konfuse <897896450@qq.com>
2019-04-15 09:34:56 +0800

    添加启动类

---
+++ zfbd/src/main/java/project/zfbd/Application.java
@@ -0,0 +1,15 @@
+package project.zfbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Konfuse
+ * @Date: 2019/4/15 9:32
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
